5edee4be5a1fc39fb76913baacc5292f693062a7
rom341 <roman.murzik.antpnov@gmail.com>
2023-02-24 20:52:47 +0400

    Java-mysql connection 1
    24.02.2023
    V 1.01
    Deleted unused file

--- DB_Conection_test_1/src/DBConst.java
+++
@@ -1,5 +0,0 @@
-public class DBConst {
-    public final static String userName = "root";
-    public final static String userPassword = "my-secret-pw";
-    public static final String connectionURL = "jdbc:mysql://localhost:3306/mysql";
-}
